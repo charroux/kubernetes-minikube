a608e089a02bb70f8b4146afe524d7fc122aa892
charroux <benoit.charroux@gmail.com>
2022-12-13 09:38:14 +0100

    first commit

--- MyService/src/main/java/com/example/myservice/MyServiceApplication.java
+++ MyService/src/main/java/com/example/myservice/MyServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyserviceApplication {
+public class MyServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyserviceApplication.class, args);
+		SpringApplication.run(MyServiceApplication.class, args);
 	}
 
 }
